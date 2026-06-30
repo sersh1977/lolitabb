27512b1300ea4f6a08b4c8545e14bc83eb97a7be
Sersh77 <sergiomaccio@gmail.com>
2026-06-30 15:13:52 -0300

    cleanup: eliminar archivos sin uso y rotar token de API

--- fix-dollar.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let h = fs.readFileSync('public/index.html', 'utf8');
-let count = 0;
-// Fix $${ -> ${ in template literals
-h = h.replace(/\$\$\{fmtMoney\(/g, () => { count++; return '${fmtMoney('; });
-fs.writeFileSync('public/index.html', h, 'utf8');
-console.log('Dobles $ corregidos:', count);
-console.log('Dobles $ restantes:', (h.match(/\$\$\{fmtMoney\(/g)||[]).length);
